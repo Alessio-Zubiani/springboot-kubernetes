8d56ede7c4e9d553848865758bd625901cfdc085
4631 <alessio.zubiani@popso.it>
2024-06-05 18:29:43 +0200

    Added JIB configuration

--- src/main/java/com/example/kubernetes/entity/Employee.java
+++ src/main/java/com/example/kubernetes/entity/Employee.java
@@ -25,7 +25,7 @@ import lombok.ToString;
 public class Employee {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
 	@Column(name = "name")
