0649a425e3896a9ea7e0e54c4c4afc24df5e9658
4631 <alessio.zubiani@popso.it>
2024-06-05 20:30:54 +0200

    Added JIB configuration

--- src/main/java/com/example/kubernetes/controller/EmployeeController.java
+++ src/main/java/com/example/kubernetes/controller/EmployeeController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,12 +25,7 @@ public class EmployeeController {
 	
 	
 	@PostMapping
-	public ResponseEntity<Employee> createEmployee() {
-		Employee e = Employee.builder()
-				.name("Alessio")
-				.surname("Zubiani")
-				.department("IT")
-				.build();
+	public ResponseEntity<Employee> createEmployee(@RequestBody Employee e) {
 		return ResponseEntity.status(HttpStatus.OK).body(this.service.createEmployee(e));
 	}
 	
