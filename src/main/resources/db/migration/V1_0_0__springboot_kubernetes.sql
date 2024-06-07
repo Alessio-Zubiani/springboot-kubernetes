2007fb5102925681aa27aac3ca49e7cbd99dcc12
4631 <alessio.zubiani@popso.it>
2024-06-07 12:04:21 +0200

    Added Flyway configurations

--- src/main/resources/db/migration/V1_0_0__springboot_kubernetes.sql
+++ src/main/resources/db/migration/V1_0_0__springboot_kubernetes.sql
@@ -1,6 +1,6 @@
 -- Create table employee
-CREATE TABLE employee (
-	id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS employee (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(50),
 	surname VARCHAR(50),
 	department VARCHAR(50)
