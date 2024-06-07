858d1cb5f737864583e1df9b429aefc51d0c6575
4631 <alessio.zubiani@popso.it>
2024-06-07 11:25:25 +0200

    Added Flyway configurations

---
+++ src/main/java/com/example/kubernetes/flyway/FlywayMigration.java
@@ -0,0 +1,30 @@
+package com.example.kubernetes.flyway;
+
+import javax.sql.DataSource;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class FlywayMigration {
+	
+	@Bean
+	public FlywayMigrationStrategy repairAndMigrateStrategy(final DataSource dataSource) {
+		
+		Flyway.configure()
+        		.dataSource(dataSource)
+        		.baselineOnMigrate(true)
+        		.outOfOrder(true)
+        		.ignoreMigrationPatterns("*:missing")
+        		.load();
+		
+		return flyway -> {
+			flyway.repair();
+            flyway.migrate();
+		};
+	}
+	
+}
---
+++ src/main/resources/db/migration/V1_1_0_0__springboot_kubernetes.sql
@@ -0,0 +1,7 @@
+-- Create table employee
+CREATE TABLE employee (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(50),
+	surname VARCHAR(50),
+	department VARCHAR(50)
+);
