aa56c1f35f0813989c41ee28204e8b212d6b6f1f
4631 <alessio.zubiani@popso.it>
2024-06-11 09:42:03 +0200

    Added ActiveMq configurations

--- src/main/java/com/example/kubernetes/service/MessageService.java
+++ src/main/java/com/example/kubernetes/service/MessageService.java
@@ -1,11 +1,12 @@
 package com.example.kubernetes.service;
 
 import com.example.kubernetes.entity.Employee;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 import jakarta.jms.JMSException;
 
 public interface MessageService {
 	
-	void sendMessage(Employee e) throws JMSException;
+	void sendMessage(Employee e) throws JMSException, JsonProcessingException;
 
 }
--- src/main/java/com/example/kubernetes/service/MessageServiceImpl.java
+++ src/main/java/com/example/kubernetes/service/MessageServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.kubernetes.service;
 
 import com.example.kubernetes.entity.Employee;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
@@ -16,12 +18,15 @@ import lombok.extern.slf4j.Slf4j;
 public class MessageServiceImpl implements MessageService {
 
 	private final JmsTemplate jmsTemplate;
+	private final ObjectMapper mapper;
 	
 	
 	@Override
-	public void sendMessage(Employee e) throws JMSException {
+	public void sendMessage(Employee e) throws JMSException, JsonProcessingException {
+		String message = this.mapper.writeValueAsString(e);
 		log.info("Sending message: [{}]", e);
-		this.jmsTemplate.convertAndSend("springboot.kubernetes", e);
+		
+		this.jmsTemplate.convertAndSend("springboot.kubernetes", message);
 	}
 
 }
